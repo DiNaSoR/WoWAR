cb7530e70d0adb3930fa4d022bb1308514cbd7f9
Platine-WOW <platine.wow@gmail.com>
2024-02-24 21:21:16 +0100

    Final version, quests displaying OK

--- WoW_Arabic_Reshaper.lua
+++ WoW_Arabic_Reshaper.lua
@@ -331,196 +331,25 @@ end
 -- Reverses the order of UTF-8 letters with ReShaping; Parameters: s=arabic UTF8 text
 -------------------------------------------------------------------------------------------------------
 function AS_UTF8reverse(s)
-   local newstr = "";
-   if (s) then                         -- check if argument is not empty (nil)
-      local bytes = strlen(s);
-      local pos = 1;
-      local char0 = '';
-      local char1, char2, char3;
-      local charbytes1, charbytes2, charbytes3;
-      local position = -1;             -- not specified
-      local nextletter = 0;
-      local spaces = '~`^&*[]+/%<>( )?$#%؟!,.;:،|"-'; -- letters that we treat as a space
+   if not s or #s == 0 then return "" end  -- Check if string is empty or nil
 
-      while (pos <= bytes) do
-         charbytes1 = AS_UTF8charbytes(s, pos);        -- count of bytes (liczba bajtów znaku)
-         char1 = strsub(s, pos, pos + charbytes1 - 1); -- current character
-         pos = pos + charbytes1;
-
-         if (pos <= bytes) then
-            charbytes2 = AS_UTF8charbytes(s, pos);                                     -- count of bytes (liczba bajtów znaku)
-            char2 = strsub(s, pos, pos + charbytes2 - 1);                              -- next character
-            if (pos + charbytes2 <= bytes) then                                        -- 3rd next letter is available
-               charbytes3 = AS_UTF8charbytes(s, pos + charbytes2);                     -- count of bytes (liczba bajtów znaku)
-               char3 = strsub(s, pos + charbytes2, pos + charbytes2 + charbytes3 - 1); -- 3rd next character
-            else
-               charbytes3 = 0;
-               char3 = 'X';
-            end
-
-            if (AS_UTF8find(spaces, char2)) then
-               nextletter = 1;      -- space, question mark, exclamation mark, comma, dot, etc.
-            else
-               nextletter = 2;      -- normal letter
-            end
-         else
-            nextletter = 0;         -- no more letters
-            char2 = 'X';
-            char3 = 'X';
-            charbytes2 = 0;
-            charbytes3 = 0;
-         end
-
-         -- first determine the original position of the letter in the word
-         if (AS_UTF8find(spaces, char1)) then
-            position = -1;                                                   -- space, question mark, exclamation mark, comma, dot, etc.
-         elseif (position < 0) then                                          -- not specified yet (start the word)
-            if ((nextletter == 0) or (nextletter == 1)) then                 -- end of file or space on as a next letter
-               position = 0;                                                 -- isolated letter
-            else
-               position = 1;                                                 -- initial letter
-            end
-         elseif ((position == 0) or (position == 1) or (position == 2)) then -- it was isolated or initial or middle letter
-            if ((nextletter == 0) or (nextletter == 1)) then                 -- end of file or space on next letter
-               position = 3;                                                 -- final letter
-            else
-               if (position == 0) then                                       -- it was isolated letter
-                  position = 1;                                              -- initial letter
-               else
-                  position = 2;                                              -- middle letter
-               end
-            end
-         else                                                                -- it was final letter (position == 3)
-            position = -1;
-         end
-
-         -- now modifications to the form of the letter depending on the preceding special letters
-         if ((char0 == "\239\187\188") or (char0 == "\239\187\184") or (char0 == "\239\187\186") or
-                (char0 == "\239\187\182") or (char0 == "\216\167") or (char0 == "\216\163") or
-                (char0 == "\216\165") or (char0 == "\216\162") or (char0 == "\239\187\183") or
-                (char0 == "\239\187\185") or (char0 == "\239\187\181") or (char0 == "\239\187\181\216\167") or
-                (char0 == "\239\187\183\216\167") or (char0 == "\239\187\185\216\167") or (char0 == "\239\187\187\216\167") or
-                (char0 == "\217\132" and char1 == "\216\167") or (char0 == "\239\187\187")) then -- previous letter was ALEF, DA, THA, RA, ZAI, WA or LA, current should be in isolated form, only if this letter is the last in the word, otherwise form must be initial
-            if (AS_UTF8find(spaces, char1)) then                                                 -- current character is space
-               position = 0;                                                                     -- isolated letter
-            elseif ((nextletter == 0) or (nextletter == 1)) then                                 -- end of file or space on as a next letter OR letter is ALEF
-               position = 0;                                                                     -- isolated letter
-            else
-               position = 1;                                                                     -- initial letter
-            end
-         elseif (char0 == "\216\176") and (char1 == "\217\135") then                             -- previous letter was THA, current HA should be in isolated form, only if HA is the last in the word, otherwise form must be initial
-            if ((nextletter == 0) or (nextletter == 1)) then                                     -- end of file or space on as a next letter
-               position = 0;                                                                     -- isolated letter
-            else
-               position = 1;                                                                     -- initial letter
-            end
-         elseif (char0 == "\216\175") or (char0 == "\216\176") or (char0 == "\216\177") or
-             (char0 == "\216\178") or (char0 == "\217\136") or (char0 == "\216\164") then
-            if (AS_UTF8find(spaces, char1)) then                 -- current character is space
-               position = 0;                                     -- isolated letter
-            elseif ((nextletter == 0) or (nextletter == 1)) then -- next character is space
-               position = 0;                                     -- isolated letter
-            else
-               position = 1;                                     -- initial letter
-            end
-         end
-
-         if (char0 == "\216\161") then  -- Hamza character
-            if nextletter == 0 then     -- No more characters after Hamza
-               position = 0             -- Isolated form
-            elseif nextletter == 1 then -- If next character is a space
-               -- Check the character after the space
-               if pos + charbytes2 <= bytes then
-                  local nextCharBytes = AS_UTF8charbytes(s, pos + charbytes2)
-                  local nextChar = strsub(s, pos + charbytes2, pos + charbytes2 + nextCharBytes - 1)
-
-                  -- Regardless of the nature of the next character, set to isolated form
-                  position = 0
-               else
-                  -- If it's the end of the string, set to isolated form
-                  position = 0
-               end
-            else            -- If next character is not a space
-               position = 0 -- Isolated form
-            end
-         end
-
-         if ((AS_Reshaping_Rules3[char1 .. char2 .. char3]) and (position >= 0)) then -- ligature 3 characters
-            local ligature = AS_Reshaping_Rules3[char1 .. char2 .. char3];
-            if (position == 0) then
-               char1 = ligature.isolated;
-            elseif (position == 1) then
-               char1 = ligature.initial;
-            elseif (position == 2) then
-               char1 = ligature.middle;
-            else
-               char1 = ligature.final;
-            end
-            pos = pos + charbytes2 + charbytes3;                                 -- we omit the next preceding letters
-         elseif ((AS_Reshaping_Rules2[char1 .. char2]) and (position >= 0)) then -- ligature 2 characters
-            local ligature = AS_Reshaping_Rules2[char1 .. char2];
-            if (position == 0) then
-               char1 = ligature.isolated;
-            elseif (position == 1) then
-               char1 = ligature.initial;
-            elseif (position == 2) then
-               char1 = ligature.middle;
-            else
-               char1 = ligature.final;
-            end
-            pos = pos + charbytes2; -- we omit the next preceding letter
-         end
-
-         -- check if the character has reshaping rules
-         local rules = AS_Reshaping_Rules[char1];
-         if (rules) then
-            -- apply reshaping rules based on the character's position in the string
-            if (position == 0) then -- isolated letter
-               if (debug_show_form == 1) then
-                  newstr = '0' .. rules.isolated .. newstr;
-               else
-                  newstr = rules.isolated .. newstr;
-               end
-            elseif (position == 1) then -- initial letter
-               if (debug_show_form == 1) then
-                  newstr = '1' .. rules.initial .. newstr;
-               else
-                  newstr = rules.initial .. newstr;
-               end
-            elseif (position == 2) then -- middle letter
-               if (debug_show_form == 1) then
-                  newstr = '2' .. rules.middle .. newstr;
-               else
-                  newstr = rules.middle .. newstr;
-               end
-            else -- final letter
-               if (debug_show_form == 1) then
-                  newstr = '3' .. rules.final .. newstr;
-               else
-                  newstr = rules.final .. newstr;
-               end
-            end
-         else                      -- character has no reshaping rules, add it to the result string as is
-            if (char1 == "<") then -- we need to reverse the directions of the parentheses
-               char1 = ">";
-            elseif (char1 == ">") then
-               char1 = "<";
-            elseif (char1 == "(") then
-               char1 = ")";
-            elseif (char1 == ")") then
-               char1 = "(";
-            end
-            if (debug_show_form == 1) then
-               newstr = position .. char1 .. newstr;
-            else
-               newstr = char1 .. newstr;
-            end
-         end
-         
-         char0 = char1; --save to previous letter
-      end
+   local newstrParts = {};  -- Use table to collect parts of the new string
+   local bytes = strlen(s);
+   local index = 1;         -- Keep track of insert position in table
+   local pos = 1;
+   while pos <= bytes do
+      local charbytes = AS_UTF8charbytes(s, pos)
+      local char1 = strsub(s, pos, pos + charbytes - 1)
+      newstrParts[index] = char1;
+      index = index + 1;
+      pos = pos + charbytes;
    end
-   return newstr;
+   -- Reverse the collected parts and join them into a single string
+   for i = 1, math.floor(#newstrParts / 2) do
+      newstrParts[i], newstrParts[#newstrParts - i + 1] = newstrParts[#newstrParts - i + 1], newstrParts[i];
+   end
+   
+   return table.concat(newstrParts);  -- Join parts into final string
 end
 
 -------------------------------------------------------------------------------------------------------
