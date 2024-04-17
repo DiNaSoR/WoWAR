489acaf183b2ce7241d1d888d7a89215e369f692
Dragonarab <dragonarab@gmail.com>
2024-04-18 00:41:56 +0300

    fix immersion+storyline

--- WoW_Quests.lua
+++ WoW_Quests.lua
@@ -2358,11 +2358,11 @@ function QTR_Immersion_OFF()   -- wywoływanie oryginału
    ImmersionFrame.TalkBox.NameFrame.Name:SetText(QTR_quest_EN[QTR_quest_ID].title);
    ImmersionFrame.TalkBox.TextFrame.Text:SetFont(Original_Font2, 14);
    if (QTR_quest_EN[QTR_quest_ID].completion and (strlen(QTR_quest_EN[QTR_quest_ID].completion)>0)) then   -- przywróć oryginalny tekst
-      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_ExpandUnitInfo(QTR_quest_EN[QTR_quest_ID].completion,false,ImmersionFrame.TalkBox.TextFrame.Text,WOWTR_Font2));
+      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_quest_EN[QTR_quest_ID].completion);
    elseif (QTR_quest_EN[QTR_quest_ID].progress and (strlen(QTR_quest_EN[QTR_quest_ID].progress)>0)) then
-      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_ExpandUnitInfo(QTR_quest_EN[QTR_quest_ID].progress,false,ImmersionFrame.TalkBox.TextFrame.Text,WOWTR_Font2));
+      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_quest_EN[QTR_quest_ID].progress);
    else
-      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_ExpandUnitInfo(QTR_quest_EN[QTR_quest_ID].details,false,ImmersionFrame.TalkBox.TextFrame.Text,WOWTR_Font2));
+      ImmersionFrame.TalkBox.TextFrame.Text:SetText(QTR_quest_EN[QTR_quest_ID].details);
    end
    QTR_Immersion_OFF_Static();       -- inne statyczne dane
 end
@@ -2412,42 +2412,43 @@ function QTR_Storyline_Objectives()
    if (QTR_PS["active"]=="1" and QTR_PS["storyline"]=="1" and QTR_quest_ID>0) then
       local string_ID= tostring(QTR_quest_ID);
       Storyline_NPCFrameObjectivesContent.Title:SetText(QTR_ReverseIfAR(WoWTR_Localization.objectives));
+      Storyline_NPCFrameObjectivesContent.Title:SetFont(WOWTR_Font1, 13);
       if (QTR_QuestData[string_ID] ) then
-         Storyline_NPCFrameObjectivesContent.Objectives:SetText(QTR_ExpandUnitInfo(QTR_QuestData[string_ID]["Objectives"],true,Storyline_NPCFrameObjectivesContent.Objectives,WOWTR_Font2));
+         Storyline_NPCFrameObjectivesContent.Objectives:SetText(QTR_ExpandUnitInfo(QTR_QuestData[string_ID]["Objectives"],true,Storyline_NPCFrameObjectivesContent.Objectives,WOWTR_Font2,-40));
          Storyline_NPCFrameObjectivesContent.Objectives:SetFont(WOWTR_Font2, 13);
       end   
       if (Storyline_RewardsHeader0) then
          Storyline_RewardsHeader0:SetText(QTR_ReverseIfAR(QTR_quest_LG[QTR_quest_ID].itemreceive));
-         Storyline_RewardsHeader0:SetFont(WOWTR_Font2, 13);
+         Storyline_RewardsHeader0:SetFont(WOWTR_Font1, 13);
       end
       if (Storyline_RewardsHeader1) then
          if (Storyline_RewardsHeader1:GetText() == REWARD_AURA) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_aura));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_SPELL) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_spell));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_COMPANION) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_companion));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_FOLLOWER) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_follower));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_REPUTATION) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_reputation));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_TITLE) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_title));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_TRADESKILL) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_tradeskill));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_UNLOCK) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_unlock));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          elseif (Storyline_RewardsHeader1:GetText() == REWARD_BONUS) then
             Storyline_RewardsHeader1:SetText(QTR_ReverseIfAR(QTR_Messages.reward_bonus));
-            Storyline_RewardsHeader1:SetFont(WOWTR_Font2, 13);
+            Storyline_RewardsHeader1:SetFont(WOWTR_Font1, 13);
          end
       end
    end
@@ -2466,14 +2467,14 @@ end
 function QTR_Storyline(nr)
    if (QTR_PS["transtitle"]=="1") then
       Storyline_NPCFrame.Banner.Title:SetText(QTR_ReverseIfAR(QTR_quest_LG[QTR_quest_ID].title));
-      Storyline_NPCFrame.Banner.Title:SetFont(WOWTR_Font2, 18);
+      Storyline_NPCFrame.Banner.Title:SetFont(WOWTR_Font1, 18);
    end
    local string_ID= tostring(QTR_quest_ID);
    local texts = { "" };
    if ((Storyline_NPCFrameChat.event ~= nil) and (QTR_QuestData[string_ID] ~= nil))then
       local event = Storyline_NPCFrameChat.event;
       if (event=="QUEST_DETAIL") then
-     	   texts = { strsplit("\n", QTR_ExpandUnitInfo(QTR_QuestData[string_ID]["Description"],false,Storyline_NPCFrameChatText,WOWTR_Font2)) };
+     	   texts = { strsplit("\n", QTR_ExpandUnitInfo(QTR_QuestData[string_ID]["Description"],false,Storyline_NPCFrameChatText,WOWTR_Font2,-15)) };
       end   
       if (event=="QUEST_PROGRESS") then
      	   texts = { strsplit("\n", QTR_ExpandUnitInfo(QTR_QuestData[string_ID]["Progress"],false,Storyline_NPCFrameChatText,WOWTR_Font2)) };
@@ -3353,10 +3354,10 @@ end
 
 function WOWTR_AnsiReverse(txt)
    local text = txt
-   if (WoWTR_Localization.lang == 'AR') then    -- used to reverse a player's name, class, and race.
-      text = string.reverse(text);
+   if (WoWTR_Localization.lang == 'AR') then
+      text = string.reverse(text)
    end
-   return text;
+   return text
 end
 
 -------------------------------------------------------------------------------------------------------------------
